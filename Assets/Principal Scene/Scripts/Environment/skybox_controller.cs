ac34194e8cb0d17eaf7c76e70a96b5fe541cad43
Mauro Castillo <mauroctm7@gmail.com>
2018-12-05 20:06:15 -0500

    updeta folder

---
+++ Assets/Principal Scene/Scripts/Environment/skybox_controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skybox_controller : MonoBehaviour {
+    //Gamer Controller
+    public GameController game_controller;
+    // Use this for initialization
+    public Material skybox_morning;
+    public Material skybox_afternoon;
+    public Material skybox_night;
+    public Light principal_light;
+    void Start () {
+        Skybox_update();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Skybox_update() {
+        int hours = game_controller.getHours();
+        if (hours >= 7 && hours < 13) {
+            RenderSettings.skybox = skybox_morning;
+            principal_light.intensity = 1.0f;
+            Debug.Log("Mornig");
+        }
+        if (hours >= 13 && hours < 19)
+        {
+            RenderSettings.skybox = skybox_afternoon;
+            principal_light.intensity = 0.7f;
+            Debug.Log("Afternoon");
+        }
+        if (hours >= 19 || hours <= 6)
+        {
+            RenderSettings.skybox = skybox_night;
+            principal_light.intensity = 0.2f;
+            Debug.Log("Night");
+        }
+
+    }
+}
---
+++ Assets/Principal Scene/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameController : MonoBehaviour {
+    /* This is time generl of game */
+    private DateTime time = DateTime.Now;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int getHours() {
+        return 14;
+        //return this.time.Hour;
+    }
+
+    public int getMinute()
+    {
+        return this.time.Minute;
+    }
+
+}
